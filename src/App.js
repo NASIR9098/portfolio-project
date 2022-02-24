ff4388bdad01a251f4e0ef9862275c5323f634f3
nasir9098 <nasiralics99@gmail.com>
2022-02-24 11:38:13 +0500

    project setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+     <p>This  is the home page</p>
+     <div className='container'>
+       <div className='row'>
+         <div className='col-4'>This is the col 4</div>
+         <div className='col-4'>This is the col 4</div>
+         <div className='col-4'>This is the col 4</div>
+       </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
